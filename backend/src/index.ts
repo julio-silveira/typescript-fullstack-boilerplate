fdfdff216db2653e9941f76e340f626b002d469b
julio-silveira <julioqueiroz15@gmail.com>
2022-11-19 15:20:53 -0300

    feat: added error middleware and some libs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
-import express from 'express';
+import express, { NextFunction,Request, Response} from 'express';
 import statusCodes from './statusCodes';
+import 'express-async-errors';
+
 
 const app = express();
 
@@ -7,9 +9,34 @@ app.use(express.json());
 
 const PORT = 8000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.status(statusCodes.OK).send('Express + TypeScript')
 });
+
+app.use(( err: Error, req: Request, res: Response, next: NextFunction) =>{
+  const {name, message, details } =  err as any;
+  console.log(`name: ${name}`);
+
+  switch (name) {
+    case 'BadRequestError':
+      res.status(400).json({message})
+      break
+    case 'ValidationError': 
+      res.status(400).json({message: details[0].message})
+      break
+    case 'NotFoundError': 
+      res.status(404).json({message})
+      break
+    case 'ConflictError':
+      res.status(409).json({message});
+      break
+    default:
+      console.error(err);
+      res.sendStatus(500)
+  }
+
+  next();
+}) 
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
